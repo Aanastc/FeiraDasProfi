38fb03d2516ba073ce03337241c46099d439a58c
Ana <ananovos@hotmail.com>
2024-05-11 21:16:01 -0300

    import use effect

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import SetaAcessar from "../assets/imgs/CapasHome/SetaAcessar.png";
